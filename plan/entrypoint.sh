0e527c428c6d8bd58ca8b60c822fb13ab6f09005
Luke Kysow <lkysow@gmail.com>
2018-10-09 10:03:45 -0500

    Reorganize

--- plan/entrypoint.sh
+++ plan/entrypoint.sh
@@ -9,10 +9,11 @@ terraform workspace select "$WORKSPACE"
 PLAN_OUTPUT_FILE=$(mktemp)
 sh -c "TF_IN_AUTOMATION=true terraform plan -no-color $* | tee $PLAN_OUTPUT_FILE"
 
-COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
 FMT_PLAN=$(cat "$PLAN_OUTPUT_FILE" | sed -r -e 's/^  \+/\+/g' | sed -r -e 's/^  ~/~/g' | sed -r -e 's/^  -/-/g')
 COMMENT="\`\`\`diff
 $FMT_PLAN
 \`\`\`"
 PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
+COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
+
 curl -v -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL"
